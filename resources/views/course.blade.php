0279d6f68f8d23ebc5292beb9345665fd440f35d
Sina <110523328+Sinasaharkhiiz@users.noreply.github.com>
2023-07-06 21:53:29 +0330

    v3

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -23,7 +23,7 @@
         </div>
         
         <div class="form-group">
-        <h3  style=" margin-right:20px;margin-top:50px;">منتشر کننده : {{$p_data->name}}</h3>
+        <h3  style=" margin-right:20px;margin-top:50px;">.</h3>
         </div>
         <hr>
         <div class="form-group">
